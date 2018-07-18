e5a3a312aaa68a07e9cefc8ce9d5fd2759c9227c
Karl-Dell <karl.consing@gmail.com>
2018-07-18 00:13:25 -0500

    Intro to JavaScript

---
+++ Files/JS Stalker Exercise/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("Waht is your last name?");
+var personAge = prompt("What is your age?");
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + personAge + " years old")
